f6b6dfd52642cc5fb3e1e4d1e074e3a4cb704611
Dawson Schroeder <schroeder.dawson.d@gmail.com>
2023-04-03 11:46:13 -0500

    added some basic layout

--- js/movies-js-dc.js
+++ js/movies-js-dc.js
@@ -1,1 +1,36 @@
-import * as movies from "./movies-module.js"
+import {getMovies} from "./movies-module.js"
+
+
+(async()=>{
+
+    let movies = await getMovies();
+    console.log(movies);
+
+
+})();
+
+
+
+
+
+let pageWrapper = document.querySelector('.page-wrapper');
+
+let dropdowns = document.querySelectorAll('[data-dropdown="parent"]');
+dropdowns.forEach(function(dropdown){
+    let toggle = dropdown.querySelector('[data-dropdown="toggle"]');
+    toggle.addEventListener('click', function(){
+        dropdowns.forEach(function(element){
+            if(element.classList.contains('open') && element !== dropdown) {
+                element.classList.remove('open')
+            }
+        })
+        dropdown.classList.toggle('open');
+    });
+});
+pageWrapper.addEventListener('click', function(event){
+    if (!event.target.closest('[data-dropdown="parent"]')) {
+        dropdowns.forEach(function(dropdown){
+            dropdown.classList.remove('open');
+        });
+    }
+})
